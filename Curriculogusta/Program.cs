356736a999d273d2705b5288a91bdfe123d2aac3
gustavo miranda <gustavo.miranda43@etec.sp.gov.br>
2022-11-07 21:37:07 -0300

    gravaçao inicial

---
+++ Curriculogusta/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curriculogusta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string nome;
+			int nascimento;
+			int rg;
+			int cpf;
+			string genero;
+			string endereço;
+			int numero;
+			int cep;
+			int telefone;
+			string experiência;
+			string formaçao;
+			string objetivo;
+			string qualificaçao;
+
+
+
+			Console.WriteLine("Digite seu nome:");
+		nome: Console.ReadLine();
+
+			Console.WriteLine("Digite dus data de nascimento:");
+		nascimento: Console.ReadLine();
+
+			Console.WriteLine("Digite o numero do seu RG:");
+		rg: Console.ReadLine();
+
+			Console.WriteLine("Digite o numero do seu CPF:");
+		cpf: Console.ReadLine();
+
+			Console.WriteLine("Qual o seu gênero");
+		genero: Console.ReadLine();
+
+			Console.WriteLine("Digite seu endereço");
+		endereço: Console.ReadLine();
+
+			Console.WriteLine("Digite o numero de sua residência");
+		numero: Console.ReadLine();
+
+			Console.WriteLine("Digite seu CEP");
+		cep: Console.ReadLine();
+
+			Console.WriteLine("Digite o numero de seu celular");
+		telefone: Console.ReadLine();
+
+			Console.WriteLine("Nos apresente sua experiência profissional");
+		experiencia: Console.ReadLine();
+
+			Console.WriteLine("Digite sua formação:");
+		formaçao: Console.ReadLine();
+
+			Console.WriteLine("Qual o seu objetivo profissional?");
+		objetivo: Console.ReadLine();
+
+			Console.WriteLine("Apresete-nos suas habilidades e qualificações para o cargo");
+		qualificaçao: Console.ReadLine();
+
+			Console.WriteLine("Obrigado pela preferência, em breve retornaremos!");
+
+
+			Console.ReadKey();
+
+
+		}
+	}
+}
